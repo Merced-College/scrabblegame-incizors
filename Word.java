41376debed41e055622516ccdc327d590d16cc58
incizors <thatoneguywhoscool@gmail.com>
2025-07-05 06:07:32 -0700

    Create Word.java
    Added a Word object class with the help of AI.

---
+++ Word.java
@@ -0,0 +1,21 @@
+public class Word implements Comparable<Word> {
+    private String text;
+
+    public Word(String text) {
+        this.text = text.toUpperCase(); // Store in uppercase for consistency
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public int compareTo(Word other) {
+        return this.text.compareTo(other.text);
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+}
